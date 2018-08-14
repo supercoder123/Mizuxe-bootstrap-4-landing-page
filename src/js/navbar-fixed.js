32f6ad4cec609169b98468f312a6f3d7a86a9bf1
Ashley Fernandes <ashleefernandes@gmail.com>
2018-08-14 19:58:27 +0530

    navbar-fixed supports navbar collapse closing

--- src/js/navbar-fixed.js
+++ src/js/navbar-fixed.js
@@ -4,7 +4,7 @@
 */
 
 // init nav object from dom
-var nav = $('nav');
+var nav = $("nav");
 // get heigth of the nav
 var navHeight = nav.outerHeight();
 
@@ -12,6 +12,7 @@ var navHeight = nav.outerHeight();
 $('a[href*="#"]:not([href="#"])').click(function(event) {
   scrollToSection(this);
   event.preventDefault();
+  $(".navbar-collapse").collapse("hide");
 });
 
 // scroll-trigger
@@ -22,15 +23,18 @@ $(document).scroll(function() {
 // get target position and scrolls to it
 function scrollToSection(self) {
   // get the target href
-  var href = $(self).attr('href');
+  var href = $(self).attr("href");
 
   // get the target position
   var targetPos = $(href).offset().top - navHeight + 5;
 
   // scroll to target
-  $('html, body').animate({
-    scrollTop: targetPos
-  }, 400);
+  $("html, body").animate(
+    {
+      scrollTop: targetPos
+    },
+    400
+  );
 }
 
 /*
@@ -48,7 +52,7 @@ function activateCurrentSection() {
   var id; // init the id of the element that will be activated
 
   // get all sections
-  var sections = $('.section');
+  var sections = $(".section");
 
   // store current position on the page when scroll is triggered
   var pos = $(document).scrollTop();
@@ -58,19 +62,23 @@ function activateCurrentSection() {
   * make it active when 50% of it is visible.
   * Otherwise the last section would never activate.
   */
-  var lastSection = sections[sections.length-1];  // get last section
+  var lastSection = sections[sections.length - 1]; // get last section
   var lastSectionTooSmall = $(lastSection).height() < $(window).height();
 
   if (lastSectionTooSmall) {
     var lastSectionTopPos = $(lastSection).offset().top;
     // lastSectionTriggerPos is true if 50% of the last section is visible
-    var lastSectionTriggerPos = $(window).height() + $(document).scrollTop() - ($(lastSection).height()/2);
+    var lastSectionTriggerPos =
+      $(window).height() +
+      $(document).scrollTop() -
+      $(lastSection).height() / 2;
     var lastSectionInView = lastSectionTriggerPos > lastSectionTopPos;
   }
 
   if (lastSectionTooSmall && lastSectionInView) {
-    id = $(lastSection).attr('id');
-  } else {  // else last section is >= 100% of the view check all sections to find the top one
+    id = $(lastSection).attr("id");
+  } else {
+    // else last section is >= 100% of the view check all sections to find the top one
     sections.each(function() {
       var top = $(this).offset().top - navHeight; // get the top & bottom position of the section
       var bottom = top + $(this).outerHeight();
@@ -81,7 +89,7 @@ function activateCurrentSection() {
       * it is the active section.
       */
       if (pos >= top && pos <= bottom) {
-        id = $(this).attr('id');       // store the id of this section
+        id = $(this).attr("id"); // store the id of this section
       }
     });
   }
@@ -90,7 +98,7 @@ function activateCurrentSection() {
    if an id was set before, activate the section in the nav
    */
   if (id) {
-    nav.find('a').removeClass('active');
-    nav.find('a[href="#' + id + '"]').addClass('active');
+    nav.find("a").removeClass("active");
+    nav.find('a[href="#' + id + '"]').addClass("active");
   }
 }
